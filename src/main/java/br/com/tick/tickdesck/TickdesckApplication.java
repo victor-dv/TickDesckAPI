f80bddad6edf8d093c102709185613e2f35a9947
victor-dv <js813417@gmail.com>
2025-08-13 19:57:02 -0300

    First commit

---
+++ src/main/java/br/com/tick/tickdesck/TickdesckApplication.java
@@ -0,0 +1,13 @@
+package br.com.tick.tickdesck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickdesckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickdesckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tick/tickdesck/TickdesckApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tick.tickdesck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickdesckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
