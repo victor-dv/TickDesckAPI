d7686792b767946607b44ae3ffa1635194039d43
Jean Carlos de Conto Filho <filhojean316@gmail.com>
2025-08-18 21:45:21 -0300

    A entity de chamados criada

---
+++ src/main/java/br/com/tick/tickdesck/domain/call/CallsEntity.java
@@ -0,0 +1,35 @@
+package br.com.tick.tickdesck.domain.call;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Data
+public class CallsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int NumeroChamado;
+    @Email(message = "O campo[email] deve ser preenchido corretamente")
+    private String EmailEnvio;
+    private String UsernameEnvio;
+    private Long IdEmpresa;
+    private Long IdUsuario;
+    private int Urgencia;
+
+    @DateTimeFormat
+    private int PrevisaoSolucao;
+    @DateTimeFormat
+    private int DataFechamento;
+
+    private Long UsuarioFechamento;
+
+
+}
---
+++ src/main/java/br/com/tick/tickdesck/infraestructure/calls/CallRepository.java
@@ -0,0 +1,7 @@
+package br.com.tick.tickdesck.infraestructure.calls;
+
+public interface CallRepository {
+
+
+
+}
