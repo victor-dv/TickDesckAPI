88efa27ae10cca4f03f0edae4cc70bf92f0b909f
victor-dv <js813417@gmail.com>
2025-08-18 21:42:22 -0300

    feature - Usuario sendo criado, porém a função de validar se existe usuario com mesmo email nÃão está funcionando

--- src/main/java/br/com/tick/tickdesck/api/user/UserController.java
+++ src/main/java/br/com/tick/tickdesck/api/user/UserController.java
@@ -2,9 +2,11 @@ package br.com.tick.tickdesck.api.user;
 
 import br.com.tick.tickdesck.application.user.UserService;
 import br.com.tick.tickdesck.domain.user.UserEntity;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,7 @@ public class UserController {
     //Se o usuário já existir, lança uma exceção e retorna uma mensagem de erro
     //Se o usuário for criado com sucesso, retorna o usuário criado
     @PostMapping("/")
-    public ResponseEntity<?> createUser(UserEntity userEntity) {
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserEntity userEntity) {
         try {
             UserEntity createdUser = userService.createUser(userEntity);
             return ResponseEntity.ok(createdUser);
--- src/main/java/br/com/tick/tickdesck/domain/user/UserEntity.java
+++ src/main/java/br/com/tick/tickdesck/domain/user/UserEntity.java
@@ -13,7 +13,6 @@ import java.util.UUID;
 @Entity
 @Data
 public class UserEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
