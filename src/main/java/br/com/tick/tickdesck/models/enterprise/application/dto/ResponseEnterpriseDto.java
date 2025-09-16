1f339eb04f55c7a64bc3f7a5d516cb1dc5007ea4
victor-dv <js813417@gmail.com>
2025-09-16 20:11:18 -0300

    Çfeature - Tratando as respostas da clase enterprise

---
+++ src/main/java/br/com/tick/tickdesck/models/enterprise/application/dto/ResponseEnterpriseDto.java
@@ -0,0 +1,24 @@
+package br.com.tick.tickdesck.models.enterprise.application.dto;
+
+import br.com.tick.tickdesck.models.enterprise.domain.EnterpriseEntity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ResponseEnterpriseDto(
+        Long id,
+        String email,
+        String corporateName,
+        String fantasyName,
+        String phone
+) {
+    public static ResponseEnterpriseDto fromEnterpriseEntity(EnterpriseEntity enterpriseEntity) {
+        return new ResponseEnterpriseDto(
+                enterpriseEntity.getId(),
+                enterpriseEntity.getEmail(),
+                enterpriseEntity.getCorporateName(),
+                enterpriseEntity.getFantasyName(),
+                enterpriseEntity.getPhone()
+        );
+    }
+}
