2af75bcfae7ee3a2106f9c0752f88dc85e02cfa5
victor-dv <js813417@gmail.com>
2025-10-13 23:46:13 -0300

    feature - Refatorei sobre a action, e arrumei erros que estavam acontecendo referente ao status de um chamado e também tratei a response de uma action

---
+++ src/main/java/br/com/tick/tickdesck/models/auditoria_call/application/dto/ResponseActionDto.java
@@ -0,0 +1,40 @@
+package br.com.tick.tickdesck.models.auditoria_call.application.dto;
+
+import br.com.tick.tickdesck.models.auditoria_call.domain.ActionEntity;
+
+import java.time.LocalDateTime;
+
+public record  ResponseActionDto(
+        Long id,
+        String description,
+        LocalDateTime data,
+        UserInfo user,
+        CallInfo call
+) {
+
+    public static ResponseActionDto fromEntity(ActionEntity entity) {
+        return new ResponseActionDto(
+                entity.getId(),
+                entity.getDescription(),
+                entity.getData(),
+                new UserInfo(
+                        entity.getUser().getId(),
+                        entity.getUser().getName(),
+                        entity.getUser().getUsername(),
+                        entity.getUser().getRole().toString()
+                ),
+                new CallInfo(
+                        entity.getCallsEntity().getId(),
+                        entity.getCallsEntity().getNumberCall(),
+                        entity.getCallsEntity().getTitle(),
+                        entity.getCallsEntity().getTeam().getName(),
+                        entity.getCallsEntity().getUrgencia().toString(),
+                        entity.getCallsEntity().isStatus()
+                )
+        );
+    }
+
+    public record UserInfo(Long id, String name, String username, String role) {}
+    public record CallInfo(Long id, Integer numberCall, String title, String teamName, String urgency, Boolean status) {}
+}
+
