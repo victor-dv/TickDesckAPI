196b8d41440e81952d6219cfc0a16bb6aff451e6
victor-dv <js813417@gmail.com>
2025-10-25 08:33:27 -0300

    feature - Criando user administrador ao inicializar a aplicação

--- src/main/java/br/com/tick/tickdesck/TickdesckApplication.java
+++ src/main/java/br/com/tick/tickdesck/TickdesckApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  TickdesckApplication {
+public class   TickdesckApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TickdesckApplication.class, args);
---
+++ src/main/java/br/com/tick/tickdesck/security/AdminInitializer.java
@@ -0,0 +1,40 @@
+package br.com.tick.tickdesck.security;
+
+import br.com.tick.tickdesck.models.user.application.dto.Role;
+import br.com.tick.tickdesck.models.user.domain.UserEntity;
+import br.com.tick.tickdesck.models.user.infra.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminInitializer implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public void run(String... args) {
+
+        if (userRepository.findByEmail("admin.tiickdesck@tiickdesck.com").isEmpty()) {
+            UserEntity adminUser = new UserEntity();
+            adminUser.setName("Administrador TIICKDESCK");
+            adminUser.setUsername("ADMIN");
+            adminUser.setEmail("admin.tiickdesck@tiickdesck.com");
+            adminUser.setPassword(passwordEncoder.encode("tckdesck@123"));
+            adminUser.setRole(Role.ADMIN);
+            adminUser.setTeamEntity(null);
+            adminUser.setFirstAccess(false);
+
+            userRepository.save(adminUser);
+            System.out.println("Usuário administrador criado com sucesso!");
+
+        } else {
+            System.out.println("Usuário administrador já existe.");
+        }
+    }
+}
