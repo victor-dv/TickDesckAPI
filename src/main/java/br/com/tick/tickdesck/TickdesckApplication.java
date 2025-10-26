8e4b33b3605d43d5e49753b9a645c34d00bfcd2b
victor-dv <js813417@gmail.com>
2025-10-26 19:20:07 -0300

    feature - Criando o .env

--- src/main/java/br/com/tick/tickdesck/TickdesckApplication.java
+++ src/main/java/br/com/tick/tickdesck/TickdesckApplication.java
@@ -2,11 +2,13 @@ package br.com.tick.tickdesck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class   TickdesckApplication {
 
 	public static void main(String[] args) {
+		Dotenv.load();
 		SpringApplication.run(TickdesckApplication.class, args);
 	}
 
